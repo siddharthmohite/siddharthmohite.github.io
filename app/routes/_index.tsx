eb30f4afa3065e6987a5d0b8e9f964fa2b61523f
siddharthmohite <siddharthmohite409@gmail.com>
2025-01-16 15:53:51 -0800

    Did rebuild to correct path

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
     <div className="flex h-screen items-center justify-center bg-gray-900">
       <div className="w-full h-full flex items-center justify-center">
         <video className="w-full h-full object-cover" autoPlay loop muted>
-          <source src="public/10sec.mp4" type="video/mp4" />
+          <source src="./10sec.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
