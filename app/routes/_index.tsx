f633a712aa56604398930cc6aa1074fdec1137bd
siddharthmohite <siddharthmohite409@gmail.com>
2025-01-17 19:43:54 -0800

    Completed the upperpart of landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useRef, useState, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
+import "/app/routes/styles/index.scss"; // Import the CSS file
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,21 +9,63 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export default function Index() {
+export default function Index()
+{
+  const [dayState, setDayState] = useState("");
+  const [ monthState, setMonthState] = useState("");
+  const [dateState, setDateState] = useState(0);
+  const [currentTimeState, setCurrentTimeState] = useState("");
+
+  const updateTime = () =>
+  {
+    const now = new Date();
+    const day = now.toLocaleString("en-US", { weekday: "long" });
+    const month = now.toLocaleString("en-US", { month: "long" });
+    const date = now.getDate();
+    const current_time = `${now.getHours() % 12 || 12}:${now.getMinutes().toString().padStart(2, "0")}`;
+
+    setDayState(day);
+    setMonthState(month);
+    setDateState(date);
+    setCurrentTimeState(current_time);
+  }
+
+  useEffect( () =>
+  {
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
+
+    return () =>
+    {
+      clearInterval(interval)
+    };
+  },[]);
+
+  
+  
 
   const videoRef = useRef<HTMLVideoElement>(null);
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
+  <div className="full-screen-container">
       <video
         ref={videoRef}
-        className="w-full h-full object-cover"
+        className="background-video"
         autoPlay
         muted
         loop={true}
       >
-      <source src="/22sec.mp4" type="video/mp4"/>
+        <source src="/22sec.mp4" type="video/mp4" />
       </video>
-    </div>
+      <div className="overlay-upperpart">
+        <div className="overlay-upperpart__textwrapper">
+            <h1 className="overlay-upperpart__heading">{dayState}, {monthState} {dateState}</h1>
+            <p className="overlay-upperpart__time">{currentTimeState}</p>
+        </div>
+      </div>
+      {/* <div className="overlay_lowerPart-container">
+
+      </div> */}
+  </div>
   );
 }
