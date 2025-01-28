c1de51e1a2dbf3bf8d3c805c4c0af1475dc7a73b
siddharthmohite <siddharthmohite409@gmail.com>
2025-01-28 01:59:58 -0800

    Added tags functionality in Finder

---
+++ app/routes/components/ColoredCircle.jsx
@@ -0,0 +1,12 @@
+export default function({fill}){
+
+
+    return(
+        <circle
+        fill={fill}
+        height="10px"
+        width="10px">
+            
+        </circle>
+    )
+}
--- app/routes/components/Finder.jsx
+++ app/routes/components/Finder.jsx
@@ -4,6 +4,10 @@ import { useState } from 'react';
 
 export default function Finder(){
 const [selectedFolderId, setSelectedFolderId] = useState(1);
+const [isExpanded, setIsExpanded] = useState(false);
+const [selectedTagId, setSelectedTagId] = useState(null);
+
+
 
 const folders = [
 
@@ -12,22 +16,35 @@ const folders = [
     {"id": 3, "text":'Projects'},
     {"id": 4, "text":'Certifications'},
     {"id": 5, "text":'Acheivements'}
-]    
+]
+
+const tags = [
+    {"id": 1, "text":'Red'},
+    {"id": 2, "text":'Orange'},
+    {"id": 3, "text":'Yellow'},
+    {"id": 4, "text":'Green'},
+    {"id": 5, "text":'Blue'},
+    {"id": 6, "text":'Purple'},
+    {"id": 7, "text":'Gray'}
+]
     return (
 
-        <div className="container">
+        <div className={`container ${isExpanded ? 'expanded' : ''}`}>
             <div className="sidebar">
                 <div className="sidebar__buttons">
                     <button className="sidebar__buttons-close">
 
                     </button>
-                    <button className="sidebar__buttons-min">
+                    <button className={`sidebar__buttons-min ${isExpanded ? 'active' : ''}`}>
                         
                     </button>
-                    <button className="sidebar__buttons-max">
+                    <button 
+                    onClick={() => setIsExpanded(!isExpanded)}
+                    className="sidebar__buttons-max">
                         
                     </button>
                 </div>
+                <div className='overflowy-wrapper'>
                 <span className="sidebar__favorites-text">
                     Favorites
                 </span>
@@ -48,6 +65,27 @@ const folders = [
                     </div>
                 ))}
                 </div>
+                <span className="sidebar__tags-text">
+                    Tags
+                </span>
+                <div className="sidebar__tag-container">
+                {tags.map((tag) => (
+                    <div
+                    title={tag.text}
+                    key={tag.id}
+                    className="sidebar__tag-holder">
+                                    <div 
+                                    onClick={() => setSelectedTagId(tag.id)}
+                                    className={`sidebar__tag-item ${selectedTagId === tag.id ? 'selected' : ''}`}>
+                                        <FolderIcon/>
+                                        <span className="sidebar__tag-item-text">
+                                            {tag.text}
+                                        </span>
+                                    </div>      
+                    </div>
+                ))}
+                </div>
+            </div>
             </div>
         </div>
     )
