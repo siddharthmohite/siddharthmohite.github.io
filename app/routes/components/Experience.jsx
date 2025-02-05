4795ddc040d7cc1b3f83971e1cc029964771ae77
siddharthmohite <siddharthmohite409@gmail.com>
2025-02-04 19:55:43 -0800

    UI changes on Finder

--- app/routes/components/Experience.jsx
+++ app/routes/components/Experience.jsx
@@ -1,4 +1,8 @@
 export default function Experience(){
+    // const 
+    const handleExpClick = () =>{
+
+    }
 
     return(
         <>
@@ -10,18 +14,23 @@ export default function Experience(){
             <div className="info-bar__divider-vertical"></div>
             <span className="info-bar__skills">Skills</span>
             <div className="info-bar__divider-vertical"></div>
-            <span className="info-bar__date-started">Date Started</span>
+            <span className="info-bar__date-started">Start Date</span>
+            <div className="info-bar__divider-vertical"></div>
+            <span className="info-bar__date-started">End Date</span>
         </div>
         <div className="divider-horizontal"></div>
         <div className="exp-container">
 
-                    <div className="exp-holder">
+                    <div 
+                    onClick={handleExpClick}
+                    className="exp-holder">
                         <div className="exp-holder__img-container">
                             <img className="exp-holder__img" src="/occourt.jpeg"/>
                         </div>
-                        <span className="exp-holder__text">Orange County Superior Court</span>
-                        <span className="exp-holder__text">C++,Python, FastAPI</span>
-                        <span className="exp-holder__text">May 31 2024</span>
+                        <span className="exp-holder__text-name">Orange County Superior Court</span>
+                        <span className="exp-holder__text-skills">C++,Python, FastAPI</span>
+                        <span className="exp-holder__text-dates">May 31 2024</span>
+                        <span className="exp-holder__text-datee">Present</span>
                     </div>
 
         </div>
